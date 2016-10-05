917428c31405b5962f52b2a0e65659ebff895cc6
Federico Holgado <federico@mailchimp.com>
2016-10-04 22:30:36 -0400

    clean up, now working reliably

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ module.exports = function(homebridge){
 }
 
 function SimpliSafeSecuritySystemAccessory(log, config) {
-
     this.log = log;
 
+    this.httpMethod = config["http_method"] || "GET";
     this.auth = {
         username: config.auth.username,
         password: config.auth.password,
@@ -18,103 +18,95 @@ function SimpliSafeSecuritySystemAccessory(log, config) {
 
     this.name = config["name"];
 
-    this.convertSimpliSafeStateToHomeKitState = function(simpliSafeState) {
-        switch (simpliSafeState) {
-            case "home":
-                return Characteristic.SecuritySystemCurrentState.STAY_ARM;
-            case "away":
-                return Characteristic.SecuritySystemCurrentState.AWAY_ARM;
-            case "off":
-                return Characteristic.SecuritySystemCurrentState.DISARMED;
-            default:
-                return 3;
+    this.convertHomeKitStateToSimpliSafeState = function(homeKitState) {
+        switch (homeKitState) {
+            case Characteristic.SecuritySystemTargetState.STAY_ARM:
+            case Characteristic.SecuritySystemTargetState.NIGHT_ARM:
+                return "home";
+                break;
+            case Characteristic.SecuritySystemTargetState.AWAY_ARM :
+                return "away";
+                break;
+            case Characteristic.SecuritySystemTargetState.DISARM:
+                return "off";
+                break;
         };
     };
 
-    this.getSimpliSafeSession = function() {
-        simplisafe({ user: this.auth.username, password: this.auth.password }, function (er, client) {
-            if (er) {
-                return er;
-            } else {
-                return client;
-            }
-        });
+    this.convertSimpliSafeStateToHomeKitState = function(simpliSafeState) {
+        switch (simpliSafeState) {
+            case "home":
+                return Characteristic.SecuritySystemTargetState.STAY_ARM;
+                break;
+            case "away":
+                return Characteristic.SecuritySystemTargetState.AWAY_ARM;
+                break;
+            case "off":
+                return Characteristic.SecuritySystemTargetState.DISARM;
+                break;
+        };
     };
-
 }
 
 SimpliSafeSecuritySystemAccessory.prototype = {
 
     setTargetState: function(state, callback) {
         this.log("Setting state to %s", state);
-        var state = null;
 
-        switch (state) {
-            case Characteristic.SecuritySystemTargetState.STAY_ARM:
-            case Characteristic.SecuritySystemTargetState.NIGHT_ARM:
-                state = "home";
-                break;
-            case Characteristic.SecuritySystemTargetState.AWAY_ARM :
-				state = "away";
-                break;
-            case Characteristic.SecuritySystemTargetState.DISARM:
-				state = "off";
-                break;
-        }
+        var self = this;
         // Set state in simplisafe 'off' or 'home' or 'away'
-		simplisafe({ user: this.auth.username, password: this.auth.password }, function (er, client) {
-			if (er) callback(er);
-            client.setState(state, function(error) {
-                if (error) {
-                    callback(error);
-                } else {
-                    var homeKitState = this.convertSimpliSafeStateToHomeKitState(client.info.state);
-                    this.service.setCharacteristic(Characteristic.SecuritySystemCurrentState, homeKitState);
-                    callback(null, homeKitState);
+        simplisafe({ user: this.auth.username, password: this.auth.password }, function (er, client) {
+            self.log(er, client);
+            self.log("Setting alarm state to:", state);
+            client.setState(self.convertHomeKitStateToSimpliSafeState(state), function() {
+                if (client && client.info && client.info.state) {
+                    self.log("Callback for set state state to:", client.info.state);
+                    // Important: after a successful server response, we update the current state of the system
+                    self.securityService.setCharacteristic(Characteristic.SecuritySystemCurrentState, state);
+                    callback(null, state);
+                    client.logout(function(er) {}); // Log out, clean out the connection
                 }
-                client.logout(function(er) {}); // Log out, clean out the connection
             }); // this is really slow. Like 10-to-20 seconds slow
-		}.bind(this));
+        });
     },
 
     getState: function(callback) {
+        var self = this;
         simplisafe({ user: this.auth.username, password: this.auth.password }, function (er, client) {
-            if (er) {
-                callback(er);
-            } else {
-                callback(null, this.convertSimpliSafeStateToHomeKitState(client.info.state));
+            if (client && client.info && client.info.state) {
+                self.log("getting alarm state:", client.info.state);
+                callback(null, self.convertSimpliSafeStateToHomeKitState(client.info.state));
             }
-        }.bind(this));
+        });
     },
 
     getCurrentState: function(callback) {
         this.log("Getting current state");
         this.getState(callback);
     },
+
     getTargetState: function(callback) {
         this.log("Getting target state");
         this.getState(callback);
     },
+
     identify: function(callback) {
         this.log("Identify requested!");
         callback(); // success
     },
 
     getServices: function() {
+        this.securityService = new Service.SecuritySystem(this.name);
 
-        this.service = new Service.SecuritySystem(this.name);
-
-        this.service
+        this.securityService
             .getCharacteristic(Characteristic.SecuritySystemCurrentState)
             .on('get', this.getCurrentState.bind(this));
 
-        this.service
+        this.securityService
             .getCharacteristic(Characteristic.SecuritySystemTargetState)
             .on('get', this.getTargetState.bind(this))
             .on('set', this.setTargetState.bind(this));
 
-        return [this.service];
-
+        return [this.securityService];
     }
 };
-
